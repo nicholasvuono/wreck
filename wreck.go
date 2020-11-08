c025db484755728d5ad862b068ad8cf1e4b06a60
nicholasvuono <nicholas.vuono@gmail.com>
2020-11-07 23:58:45 -0500

    Separated Concurrent Batch Logic
    separated the duration and iteration concurrent batch logic into separarte function for readability.

--- wreck.go
+++ wreck.go
@@ -19,48 +19,54 @@ type Options struct {
 
 func Batch(options Options, requests []Request) []string {
 	var responses []string
-
 	if options.Iterations != 0 && options.Duration == 0 {
-		var wg sync.WaitGroup
-		for i := 0; i < options.Iterations; i++ {
-			for i := 0; i < options.Vus; i++ {
-				wg.Add(1)
-				go func([]string) {
-					defer wg.Done()
-					batch, err := wreckhttp.Batch(requests)
-					explain(err)
-					response := batch.Send()
-					responseString := fmt.Sprintf("%v", response)
-					responses = append(responses, responseString)
-				}(responses)
-			}
-		}
+		concurrrentBatchIterations(options, requests, responses)
 	} else if options.Iterations == 0 {
-		now := time.Now()
-		after := now.Add(time.Duration(options.Duration) * time.Second)
-
-		for {
-			var wg sync.WaitGroup
-			now := time.Now()
-			for i := 0; i < options.Vus; i++ {
-				wg.Add(1)
-				go func([]string) {
-					defer wg.Done()
-					batch, err := wreckhttp.Batch(requests)
-					explain(err)
-					response := batch.Send()
-					responseString := fmt.Sprintf("%v", response)
-					responses = append(responses, responseString)
-				}(responses)
-			}
-			if now.After(after) {
-				break
-			}
-			wg.Wait()
-		}
+		concurrentBatchDuration(options, requests, responses)
 	} else {
 		err := errors.New("Error Options: Duration and Iteration cannot be used at the same time")
 		explain(err)
 	}
 	return responses
+}
+
+func concurrentBatchDuration(options Options, requests []Request, responses []string) {
+	now := time.Now()
+	after := now.Add(time.Duration(options.Duration) * time.Second)
+	for {
+		var wg sync.WaitGroup
+		now := time.Now()
+		for i := 0; i < options.Vus; i++ {
+			wg.Add(1)
+			go func([]string) {
+				defer wg.Done()
+				batch, err := wreckhttp.Batch(requests)
+				explain(err)
+				response := batch.Send()
+				responseString := fmt.Sprintf("%v", response)
+				responses = append(responses, responseString)
+			}(responses)
+		}
+		if now.After(after) {
+			break
+		}
+		wg.Wait()
+	}
+}
+
+func concurrrentBatchIterations(options Options, requests []Request, responses []string) {
+	var wg sync.WaitGroup
+	for i := 0; i < options.Iterations; i++ {
+		for i := 0; i < options.Vus; i++ {
+			wg.Add(1)
+			go func([]string) {
+				defer wg.Done()
+				batch, err := wreckhttp.Batch(requests)
+				explain(err)
+				response := batch.Send()
+				responseString := fmt.Sprintf("%v", response)
+				responses = append(responses, responseString)
+			}(responses)
+		}
+	}
 }
